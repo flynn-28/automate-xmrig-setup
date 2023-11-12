a4c096f9d59ee3624db5e45005231e18ef91e7de
flynn-28 <107883034+flynn-28@users.noreply.github.com>
2023-11-12 10:44:08 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install cmake git build-essential libuv1-dev libssl-dev libhwloc-dev
+git clone https://www.github.com/xmrig/xmrig
+cd xmrig
+mkdir build && cd build
+cmake ..
+make
+read -p "What is your monero wallet: " wallet
+clear
+read -p "What do you want to name your worker: " worker
+clear
+./xmrig -o stratum+ssl://rx.unmineable.com:443 -u XMR:$wallet.$worker#pqx4-0l0p
