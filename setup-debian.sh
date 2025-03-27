ee3736b4bb8238e9d3e195d01bac1fcc30cb7cdf
flynn-28 <107883034+flynn-28@users.noreply.github.com>
2025-03-27 05:28:56 -0400

    Update setup-debian.sh

--- setup-debian.sh
+++ setup-debian.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install cmake git build-essential libuv1-dev libssl-dev libhwloc-dev
+sudo apt install -y cmake git build-essential libuv1-dev libssl-dev libhwloc-dev
 git clone https://www.github.com/xmrig/xmrig
 cd xmrig
 mkdir build && cd build
