20af79f24d87e0b69f66ec5b7a1851e56c37227e
flynn-28 <107883034+flynn-28@users.noreply.github.com>
2025-03-27 05:29:20 -0400

    Update setup-alpine.sh

--- setup-alpine.sh
+++ setup-alpine.sh
@@ -5,8 +5,3 @@ cd xmrig
 mkdir build && cd build
 cmake ..
 make
-read -p "What is your monero wallet: " wallet
-clear
-read -p "What do you want to name your worker: " worker
-clear
-./xmrig -o stratum+ssl://rx.unmineable.com:443 -u XMR:$wallet.$worker#pqx4-0l0p
